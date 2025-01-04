01aeea5e6c45b426321258bc02fe8c45a77c05d4
André <andreperezmaselco.developer@gmail.com>
2025-01-04 11:46:04 -0300

    Update

--- src/instructions/SOP2.c
+++ src/instructions/SOP2.c
@@ -2,12 +2,12 @@
 #include <stdint.h>
 
 /* AMD RDNA3.5 */
-#include "AMD-RDNA3.5/Instructions/SOP2.h"
+#include "AMD-RDNA3.5/instructions/SOP2.h"
 
 uint32_t SOP2(const uint8_t SSRC0,
               const uint8_t SSRC1,
               const uint8_t SDST,
               const uint8_t OP) {
-    const struct SOP2 instruction = {SSRC0, SSRC1, SDST, OP, 0b10};
+    const struct SOP2 instruction = {SSRC0, SSRC1, SDST, OP, /* ENCODING */ 0b10};
     return *(uint32_t *)&instruction;
 }
