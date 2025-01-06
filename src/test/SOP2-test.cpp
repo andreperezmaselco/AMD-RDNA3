27ff3fc3b30d92bcc5e7f0db5b29b1dc246fbd1b
André <andreperezmaselco.developer@gmail.com>
2025-01-06 17:11:45 -0300

    Update S_ADD_U32 test

--- src/test/SOP2-test.cpp
+++ src/test/SOP2-test.cpp
@@ -5,7 +5,15 @@
 #include "AMD-RDNA3/instructions/SOP2.h"
 
 TEST(SOP2Test, S_ADD_U32) {
-    const uint32_t instruction = SOP2(S_ADD_U32, 0x00, 0x00, 0x00);
+    uint32_t instruction = SOP2(S_ADD_U32, 0x00, 0x00, 0x00);
+    EXPECT_EQ(instruction, 0b10000000000000000000000000000000);
 
-    /* TODO */
+    instruction = SOP2(S_ADD_U32, 0x01, 0x00, 0x00);
+    EXPECT_EQ(instruction, 0b10000000000000000000000000000001);
+
+    instruction = SOP2(S_ADD_U32, 0x00, 0x01, 0x00);
+    EXPECT_EQ(instruction, 0b10000000000000000000000100000000);
+
+    instruction = SOP2(S_ADD_U32, 0x00, 0x00, 0x01);
+    EXPECT_EQ(instruction, 0b10000000000000010000000000000000);
 }
